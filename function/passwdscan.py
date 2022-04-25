279b0ba714640c1f801ae803f772466817405440
Norah C.IV <37536343+RoziSec@users.noreply.github.com>
2022-04-25 15:34:57 +0800

    add ldap/mongodb/postgresql/vnc/memcache brute

--- function/passwdscan.py
+++ function/passwdscan.py
@@ -46,17 +46,20 @@ class WeakPassword:
                 elif brute_choice == '2':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
                     port = input('\033[0;33m[+] Please enter port\033[0m：')
-                    print('Ldap brute')
+                    os.system("python " + root_path + "/pocsuite3/cli.py -u " + host + ':' + port + " -r " + root_path +
+                              "/pocsuite3/brute/ldap_burst.py --verify")
 
                 elif brute_choice == '3':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
                     port = input('\033[0;33m[+] Please enter port\033[0m：')
-                    print('MemCache brute')
+                    os.system("python " + root_path + "/pocsuite3/cli.py -u " + host + ':' + port + " -r " + root_path +
+                              "/pocsuite3/brute/memcache_burst.py --verify")
 
                 elif brute_choice == '4':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
                     port = input('\033[0;33m[+] Please enter port\033[0m：')
-                    print('MongoDB brute')
+                    os.system("python " + root_path + "/pocsuite3/cli.py -u " + host + ':' + port + " -r " + root_path +
+                              "/pocsuite3/brute/mongodb_burst.py --verify")
 
                 elif brute_choice == '5':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
@@ -73,7 +76,8 @@ class WeakPassword:
                 elif brute_choice == '7':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
                     port = input('\033[0;33m[+] Please enter port\033[0m：')
-                    print('PostgreSQL brute')
+                    os.system("python " + root_path + "/pocsuite3/cli.py -u " + host + ':' + port + " -r " + root_path +
+                              "/pocsuite3/brute/postgresql_burst.py --verify")
 
                 elif brute_choice == '8':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
@@ -96,7 +100,8 @@ class WeakPassword:
                 elif brute_choice == '11':
                     host = input('\033[0;33m[+] Please enter host\033[0m：')
                     port = input('\033[0;33m[+] Please enter port\033[0m：')
-                    print('VNC brute')
+                    os.system("python " + root_path + "/pocsuite3/cli.py -u " + host + ':' + port + " -r " + root_path +
+                              "/pocsuite3/brute/vnc_burst.py --verify")
 
                 elif brute_choice == '0':
                     begin()
---
+++ pocsuite3/brute/mongodb_burst.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File : mongodb_burst.py
+# @Author : Norah C.IV
+# @Time : 2022/4/25 15:33
+# @Software: PyCharm
+
+
+if __name__ == '__main__':
+    pass
