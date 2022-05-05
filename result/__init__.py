b0c4c53939c708d83552ff35385207c91d43de38
Norah C.IV <37536343+RoziSec@users.noreply.github.com>
2022-05-05 16:30:56 +0800

    ……

---
+++ result/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File : __init__.py
+# @Author : Norah C.IV
+# @Time : 2022/5/5 16:30
+# @Software: PyCharm
+
+
+if __name__ == '__main__':
+    pass
